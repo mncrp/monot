6249d56e3d6ccc85fb7aed7359e4e24d48bf0308
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-01-02 17:49:44 +0900

    広告ブロック機能

---
+++ src/script/adBlock.js
@@ -0,0 +1,50 @@
+var element = document;
+document.addEventListener('DOMContentLoaded', function () {
+    adBlock();
+});
+
+window.onload = () => {
+    adBlock();
+    setTimeout(adBlock, 3000);
+    setTimeout(adBlock, 5000);
+}
+
+const adBlock = () => {
+    if (!location.hostname == "youtube.com") {
+        dt("iframe");
+    }
+    dc("yjAdImage");
+    dc("yadsOverlay");
+    da(/ad/gi);
+}
+
+const dt = (elementTag) => { //Delete tag
+    Array.prototype.slice.call(element.getElementsByTagName(elementTag)).forEach((el) => {
+        el.remove();
+    });
+}
+
+const dc = (elementClass) => { //Delete class
+    Array.prototype.slice.call(element.getElementsByClassName(elementClass)).forEach((el) => {
+        el.remove();
+    });
+}
+
+const di = (elementId) => { //Delete Id
+    Array.prototype.slice.call(element.getElementById(elementId)).forEach((el) => {
+        el.remove();
+    });
+}
+
+const dn = (elementName) => { //Delete name
+    Array.prototype.slice.call(element.getElementsByName(elementName)).forEach((el) => {
+        el.remove();
+    });
+}
+
+const da = (elementAll) => {
+    dt(elementAll);
+    dc(elementAll);
+    di(elementAll);
+    dn(elementAll);
+}
