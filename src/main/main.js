6b69a64fd94dba7c34e47e0d3e43f82089341212
Sorakime <69241694+Sorakime@users.noreply.github.com>
2022-08-23 23:43:44 +0900

    [fix]devTools修正

--- src/main/main.js
+++ src/main/main.js
@@ -66,7 +66,6 @@ function nw() {
       preload: `${directory}/preload/navigation.js`
     }
   });
-  global.win.webContents.openDevTools();
   global.win.setBackgroundColor('#efefef');
   global.win.loadFile(
     isMac ?
