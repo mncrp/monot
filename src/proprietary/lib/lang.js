a7d19ae5374677c6b61a4309ff53a6797e0446d7
Sorakime <69241694+Sorakime@users.noreply.github.com>
2023-01-08 17:02:32 +0900

    [add]今度こそ言語を設定する処理だけ完成

--- src/proprietary/lib/lang.js
+++ src/proprietary/lib/lang.js
@@ -1,13 +1,55 @@
 const directory = `${__dirname}/../..`;
 const {app} = require('electron');
 const {LowLevelConfig} = require('./config');
+const fs = require('fs');
+// available languages
+const langs = [
+  'en',
+  'ja'
+];
+let translation;
 const monotConfig = new LowLevelConfig(
   'config.mncfg'
 ).copyFileIfNeeded(
   `${directory}/default/config/config.mncfg`
 );
-if (!monotConfig.get('lang')) {
-  monotConfig.update().set('lang', 'ja').save();
+
+app.whenReady().then(initLang);
+
+function sysLocale() {
+  return langs.some(v => v === app.getLocale()) ?
+    app.getLocale() :
+    'ja';
+}
+
+function initLang(lang) {
+  const locale = app.getLocale().substring(0, 2);
+  lang = langs.some(v => v === locale) &&
+    lang === undefined ?
+    locale :
+    'ja';
+
+  console.log(lang);
+
+  // set language if "lang" matches "langs"
+  // if (langs.some(v => v === lang))
+  // monotConfig.update().set('lang', lang).save();
+
+  // set language if not already set
+  if (monotConfig.update().get('lang') === undefined)
+    monotConfig.set('lang', lang).save();
+
+  // load translations
+  translation = JSON.parse(
+    fs.readFileSync(
+      `${
+        directory
+      }/default/language/${
+        monotConfig.get('lang')
+      }.json`,
+      'utf-8'
+    )
+  ).translations;
 }
 
 module.exports = {
@@ -15,9 +57,9 @@ module.exports = {
     return app.getLocale();
   },
   setLang: (lang) => {
-    monotConfig.update().set('lang', lang).save();
+    initLang();
   },
-  getText: (lang) => {
-    return;
+  getText: (lang, inEn) => {
+    return translation;
   }
 };
